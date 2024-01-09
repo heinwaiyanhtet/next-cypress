c06d4b696b0dacf7bb6d68a6d2ffd343c1f27a5e
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-09 23:27:27 +0630

    assert

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -39,3 +39,20 @@ describe("Click an element",() => {
      cy.contains("type").click();
   })
 })
+
+
+describe("make an assertion",() => {
+  
+  it('Gets, types and asserts',() => {
+      cy.visit('https://example.cypress.io')
+
+      cy.contains('type').click()
+
+      cy.url().should('include','/commands/actions')
+
+      cy.get('.action-email').type('fake@email.com')
+
+      cy.get('.action-email').should('have.value', 'fake@email.com')
+
+  })
+})
