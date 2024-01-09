388a5c373417d057aa60ebb9813c1ebad9fe8f47
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-09 22:37:11 +0630

    my first test

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+
+
+describe("My first test",() => {
+  it("Does not do much!",() => {
+    //  expect(true).to.equal(true)
+      expect(true).to.equal(true);
+  })
+})
+
